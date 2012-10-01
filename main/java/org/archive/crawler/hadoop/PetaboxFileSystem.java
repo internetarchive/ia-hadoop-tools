500e688839d1ecc44ecddb719d415586ceee11f7
Kenji Nagahashi <kenji@archive.org>
2012-10-01 14:57:21 -0700

    let PetaboxFileSystem reopen prematurelly closed stream. removed MetaManager-based item lookup code into a separate class (historical record keeping purposes only)

--- main/java/org/archive/crawler/hadoop/PetaboxFileSystem.java
+++ main/java/org/archive/crawler/hadoop/PetaboxFileSystem.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.DateFormat;
@@ -455,11 +454,11 @@ public class PetaboxFileSystem extends FileSystem {
     StringBuilder value = new StringBuilder();
     if (user != null) {
       value.append("logged-in-user=").append(user).append("; ");
-      LOG.info("logged-in-user=" + user);
+      LOG.debug("logged-in-user=" + user);
     }
     if (sig != null) {
       value.append("logged-in-sig=").append(sig);
-      LOG.info("logged-in-sig=" + sig);
+      LOG.debug("logged-in-sig=" + sig);
     }
     LOG.debug("adding auth cookies:" + value.toString());
     msg.addHeader("Cookie", value.toString());
@@ -564,7 +563,6 @@ public class PetaboxFileSystem extends FileSystem {
   public Path getWorkingDirectory() {
     return cwd;
   }
-  public final static int METAMGR_ROWS_PER_PAGE = 200;
   
   protected static long isodatetime2timestamp(String isodatetime) {
     if (isodatetime == null) return 0;
@@ -577,169 +575,6 @@ public class PetaboxFileSystem extends FileSystem {
     }
   }
   
-  protected static long sqldatetime2timestamp(String sqldatetime) {
-    if (sqldatetime == null) return 0;
-    DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
-    try {
-      Date date = df.parse(sqldatetime);
-      return date.getTime();
-    } catch (ParseException ex) {
-      return 0;
-    }
-  }
-  
-  protected static boolean inCollection(String iid, String collections) {
-    int s = collections.indexOf(iid);
-    if (s < 0) return false;
-    int e = s + iid.length();
-    if (s == 0) {
-      return e >= collections.length() || collections.charAt(e) == ';';
-    } else {
-      return collections.charAt(s - 1) == ';' && 
-      (e >= collections.length() || collections.charAt(e) == ';');
-    }
-  }
-
-  protected URI buildMetaManagerURI(String itemid, int start) throws URISyntaxException {
-    StringBuilder params = new StringBuilder();
-    params.append("srt=identifier");
-    params.append("&ord=asc");
-    params.append("&w_collection=*").append(itemid).append("*");
-    params.append("&fs_identifier=on&fs_mediatype=on&fs_collection=on");
-    params.append("&off=").append(Integer.toString(start));
-    // getting all often results in 504 error for big collection.
-    //params.append("&lim=0"); // "all"
-    params.append("&lim=").append(Integer.toString(METAMGR_ROWS_PER_PAGE));
-    params.append("&output_format=json");
-    //return new URI("http", fsUri.getAuthority(), "/metamgr.php", params.toString(), null);
-    return new URI("http", "www.us.archive.org", "/metamgr.php", params.toString(), null);
-  }
-  
-  /**
-   * enumerate items in collection {@code itemid} with MetaManager.
-   * @param itemid
-   * @return
-   * @throws IOException
-   */
-  @SuppressWarnings("unchecked")
-  protected FileStatus[] searchItemsMetaManager(String itemid) throws IOException {
-    LOG.info("looking up items in collection " + itemid + " with metamgr");
-    List<FileStatus> result = new ArrayList<FileStatus>();
-    int start = 0;
-    // total number of results is not available in metamgr's JSON response. 
-    long numresults = Long.MAX_VALUE;
-    while (start < numresults) {
-      URI uri;
-      try {
-	uri = buildMetaManagerURI(itemid, start);
-	LOG.info("search uri=" + uri);
-      } catch (URISyntaxException ex) {
-	throw new IOException("failed to build URI for itemid=" + itemid + ", start=" + start, ex);
-      }
-      HttpGet get = new HttpGet(uri);
-      HttpParams params = get.getParams();
-      HttpConnectionParams.setConnectionTimeout(params, connectionTimeout);
-      HttpConnectionParams.setSoTimeout(params, socketTimeout);
-      setupRequest(get);
-      HttpEntity entity = null;
-      Map<String, Object> jo = null;
-      int retries = 0;
-      do {
-	if (retries > 0) {
-	  try {
-	    Thread.sleep(retryDelay);
-	  } catch (InterruptedException ex) {
-	  }
-	}
-	HttpResponse resp;
-	try {
-	  resp = client.execute(get);
-	} catch (IOException ex) {
-	  LOG.warn("connection to " + uri + " failed", ex);
-	  if (++retries > maxRetries) {
-	    throw new IOException(uri + ": retry exhausted trying to connect");
-	  }
-	  continue;
-	}
-	StatusLine st = resp.getStatusLine();
-	entity = resp.getEntity();
-	switch (st.getStatusCode()) {
-	case 200:
-	  if (retries > 0) {
-	    LOG.info(uri + ": succeeded after " + retries + " retry(ies)");
-	  }
-	  // it appears search engine often fails to return JSON formatted output despite
-	  // status code 200. detect it here.
-	  Reader reader = new InputStreamReader(entity.getContent(), "UTF-8");
-	  try {
-	    jo = (Map<String, Object>)JSON.parse(reader);
-	  } catch (IllegalStateException ex) {
-	    LOG.error("JSON.parse failed", ex);
-	    StringWriter w = new StringWriter();
-	    int c;
-	    while ((c = reader.read()) != -1) {
-	      w.write(c);
-	    }
-	    LOG.error("rest of response:" + w.toString());
-	  }
-	  reader.close();
-	  if (jo == null) {
-	    LOG.warn(uri + " returned 200, but JSON parser failed on entity");
-	    if (++retries > maxRetries) {
-	      throw new IOException(uri + ": retry exhausted on " + uri);
-	    }
-	    continue;
-	  }
-	  break;
-	case 502:
-	case 503:
-	case 504:
-	  if (entity != null)
-	    entity.getContent().close();
-	  if (++retries > maxRetries) {
-	    throw new IOException(uri + ": retry exhausted on "
-		+ st.getStatusCode() + " " + st.getReasonPhrase());
-	  }
-	  LOG.warn(uri + " failed " + st.getStatusCode() + " "
-	      + st.getReasonPhrase() + ", retry " + retries);
-	  entity = null;
-	  continue;
-	default:
-	  entity.getContent().close();
-	  throw new IOException(uri + ": " + st.getStatusCode() + " " + st.getReasonPhrase());
-	}
-      } while (jo == null);
-      // fields are returned in an array. we assume they are always in the same
-      // order as fs_* parameters appears in query URL.
-      // TODO: we could at least put a check of field names here.
-      Object[] rows = (Object[])jo.get("rows");
-      if (rows == null) {
-	break; //?
-      }
-      for (int i = 0; i < rows.length; i++) {
-	Object[] row = (Object[])rows[i];
-	if (row == null) continue; // just in case...
-	String iid = (String)row[0];
-	if (iid == null) continue;
-	// exclude collection items
-	String mediatype = (String)row[1];
-	if ("collection".equals(mediatype)) continue;
-	// collection query pattern is not specific enough. check whether item
-	// really have itemid as its collection.
-	if (!inCollection(itemid, (String)row[2])) continue;
-	
-	String publicdate = row.length > 3 ? (String)row[3] : null;
-	long mtime = sqldatetime2timestamp(publicdate);
-	Path qf = new Path(fsUri.toString(), "/" + iid);
-	LOG.debug("collection:" + itemid + " qf=" + qf);
-	FileStatus fst = new FileStatus(0, true, 2, 4096, mtime, qf);
-	result.add(fst);
-      }
-      start += rows.length;
-    }
-    LOG.info(String.format("searchItems(collection=%s): returning %d items", itemid, result.size()));
-    return result.toArray(new FileStatus[result.size()]);
-  }
   public HttpClient getHttpClient() {
     return client;
   }
@@ -824,6 +659,7 @@ public class PetaboxFileSystem extends FileSystem {
   public class HttpInputStream extends InputStream implements Seekable, PositionedReadable {
     protected URI uri;
     protected long pos;
+    protected long endpos;
     protected InputStream in;
     /**
      * maximum length of seeking by reading off instead of re-opening resource
@@ -839,6 +675,7 @@ public class PetaboxFileSystem extends FileSystem {
     public HttpInputStream(URI uri, int bufferSize) {
       this.uri = uri;
       this.pos = 0;
+      this.endpos = -1;
       this.in = null;
     }
     // Seekable
@@ -911,8 +748,16 @@ public class PetaboxFileSystem extends FileSystem {
 	// on most resources, but It is catastrophic to ignore when it fails.
 	switch (st.getStatusCode()) {
 	case 200:
+	case 206: // Partial Content
 	  if (retries > 0) {
 	    LOG.info(uri + ": succeeded after " + retries + " retry(ies)");
+	  }
+	  long clen = entity.getContentLength();
+	  if (clen < 0) {
+	    LOG.info("content-length is unavailable - no auto resume will be attempted.");
+	    endpos = -1;
+	  } else {
+	    endpos = pos + entity.getContentLength();
 	  }
 	  break;
 	case 404:
@@ -954,10 +799,22 @@ public class PetaboxFileSystem extends FileSystem {
     }
     @Override
     public int read() throws IOException {
-      if (in == null) open();
-      int b = in.read();
-      if (b != -1) this.pos++;
-      return b;
+      while (true) {
+	if (in == null) open();
+	int b = in.read();
+	if (b == -1) {
+	  // if sender closed socket prematurely, try reopening.
+	  if (endpos >= 0 && pos < endpos) {
+	    LOG.info("sender closed prematurely. rereading from " + pos);
+	    in = null;
+	    continue;
+	  }
+	  return b;
+	} else {
+	  pos++;
+	  return b;
+	}
+      }
     }
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
