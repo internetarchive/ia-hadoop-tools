74fe0b60a5490726032a0da320d97a5a394fd8f2
Ilya Kreymer <ilya@archive.org>
2013-04-19 12:35:37 -0700

    add MergeCluster job and ZipNumPartitioner!

---
+++ src/main/java/org/archive/hadoop/jobs/MergeCluster.java
@@ -0,0 +1,92 @@
+package org.archive.hadoop.jobs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.archive.hadoop.mapreduce.ZipNumAllOutputFormat;
+import org.archive.hadoop.pig.ZipNumPartitioner;
+import org.archive.hadoop.pig.ZipNumRecordReader;
+
+public class MergeCluster implements Tool {
+	
+	public static final String TOOL_NAME = "cdx-merge=sort";
+	public static final String TOOL_DESCRIPTION = "map/reduce program that merges existing CDX zipnum clusters";
+
+	Configuration conf = null;
+	
+	
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new BuildCluster(), args);
+		System.exit(res);
+	}
+
+	public Configuration getConf() {
+		return conf;
+	}
+
+	public void setConf(Configuration conf) {
+		this.conf = conf;
+	}
+	
+	
+	public static class ZipNumInputFormat extends NLineInputFormat
+	{
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		@Override
+		public RecordReader createRecordReader(
+				InputSplit genericSplit, TaskAttemptContext context)
+				throws IOException {
+			
+			return new ZipNumRecordReader();
+		}
+	}
+	
+	protected int runMerge(String inputPath, String outputPath, int numOutputParts, int numLinesPerInputSplit) throws IOException, ClassNotFoundException, InterruptedException
+	{		
+		Job job = new Job(getConf(), "cdx-merge-sort");
+		
+		Configuration conf = job.getConfiguration();
+		
+		FileInputFormat.setInputPaths(job, inputPath);
+		
+		if (numLinesPerInputSplit > 0) {
+			NLineInputFormat.setNumLinesPerSplit(job, numLinesPerInputSplit);
+		}
+		
+		Path[] parsedPaths = FileInputFormat.getInputPaths(job);
+		conf.set(ZipNumPartitioner.ZIPNUM_PARTITIONER_CLUSTER, parsedPaths[0].toString());
+ 
+		FileOutputFormat.setOutputPath(job, new Path(outputPath));
+ 
+		job.setInputFormatClass(ZipNumInputFormat.class);
+		job.setOutputFormatClass(ZipNumAllOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setPartitionerClass(ZipNumPartitioner.class);
+
+		job.setNumReduceTasks(numOutputParts);
+		
+		job.setJarByClass(MergeCluster.class);
+		return (job.waitForCompletion(true) ? 0 : 1);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		return runMerge(args[0], args[1], 10, 500);
+	}
+}
---
+++ src/main/java/org/archive/hadoop/pig/ZipNumPartitioner.java
@@ -0,0 +1,123 @@
+package org.archive.hadoop.pig;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.pig.impl.io.NullableText;
+import org.apache.pig.impl.io.NullableTuple;
+import org.archive.util.binsearch.SortedTextFile;
+import org.archive.util.iterator.CloseableIterator;
+
+public class ZipNumPartitioner extends Partitioner<NullableText, NullableTuple> implements Configurable {
+	
+	public final static String ZIPNUM_PARTITIONER_CLUSTER = "pig.zipnum.partitioner.clusterSummary";
+	
+	//protected ZipNumCluster cluster;
+	SortedTextFile summary = null;
+	
+	public ZipNumPartitioner()
+	{
+		
+	}
+	
+	protected final static String EMPTY_STRING = "";
+	
+	protected List<String> splitList = null;
+	
+	@Override
+	public int getPartition(NullableText key, NullableTuple value, int numSplits) {
+		
+		if (summary == null) {
+			return 0;
+		}
+		
+		if ((splitList == null) || (splitList.size() != numSplits)) {
+			loadSplits(numSplits);
+		}
+		
+		if (key.isNull()) {
+			return 0;
+		}
+		
+		String valueURL = (String)key.getValueAsPigType();
+		//String valueURL = (key.isNull() ? value.toString() : key.toString());
+		
+		int spaceIndex = valueURL.indexOf(' ');
+		
+		if (spaceIndex >= 0) {
+			valueURL = valueURL.substring(0, spaceIndex);
+		}
+		
+//		key.setIndex((byte)0);
+//		value.setIndex((byte)0);
+		
+		int index = binSearchSplits(valueURL);
+		return index;
+	}
+
+	protected int binSearchSplits(String key)
+	{
+		int loc = Collections.binarySearch(splitList, key);
+		if (loc < 0) {
+			loc = (loc * -1) - 2;
+			if (loc < 0) {
+				loc = 0;
+			}
+		}
+		
+		return loc;
+	}
+	
+	protected void loadSplits(int numSplits)
+	{
+		CloseableIterator<String> splitIter = null;
+		
+		try {
+			splitIter = summary.getSplitIterator(EMPTY_STRING, EMPTY_STRING, numSplits);
+			
+			splitList = new ArrayList<String>();
+			
+			while (splitIter.hasNext()) {
+				String str = splitIter.next();
+				int keyEndIndex = str.indexOf(' ');
+				if (keyEndIndex >= 0) {
+					str = str.substring(0, keyEndIndex);
+				}
+				splitList.add(str);
+				System.out.println(str);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (splitIter != null) {
+				try {
+					splitIter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public Configuration getConf() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setConf(Configuration conf) {
+		String clusterSummary = conf.get(ZIPNUM_PARTITIONER_CLUSTER);
+		
+		try {
+			summary = new SortedTextFile(clusterSummary);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
