fc246b2dad55d8aeebde460a5bab52dd6eda8f1a
Ilya Kreymer <ilya@archive.org>
2013-04-28 12:54:47 -0700

    add streaming ZipNumPartitioner

---
+++ src/main/java/org/archive/hadoop/streaming/ZipNumPartitioner.java
@@ -0,0 +1,114 @@
+package org.archive.hadoop.streaming;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Partitioner;
+import org.archive.util.binsearch.SortedTextFile;
+import org.archive.util.iterator.CloseableIterator;
+
+public class ZipNumPartitioner<K> implements Partitioner<K, Text> {
+	
+	public final static String ZIPNUM_PARTITIONER_CLUSTER = "pig.zipnum.partitioner.clusterSummary";
+	
+	//protected ZipNumCluster cluster;
+	SortedTextFile summary = null;
+	
+	public ZipNumPartitioner()
+	{
+		
+	}
+	
+	protected final static String EMPTY_STRING = "";
+	
+	protected List<String> splitList = null;
+	
+	@Override
+	public int getPartition(K key, Text value, int numSplits) {
+		
+		if (summary == null) {
+			return 0;
+		}
+		
+		if ((splitList == null) || (splitList.size() != numSplits)) {
+			loadSplits(numSplits);
+		}
+		
+		String valueURL = value.toString();
+		//String valueURL = (String)key.getValueAsPigType();
+		//String valueURL = (key.isNull() ? value.toString() : key.toString());
+		
+		int spaceIndex = valueURL.indexOf(' ');
+		
+		if (spaceIndex >= 0) {
+			valueURL = valueURL.substring(0, spaceIndex);
+		}
+		
+//		key.setIndex((byte)0);
+//		value.setIndex((byte)0);
+		
+		int index = binSearchSplits(valueURL);
+		return index;
+	}
+
+	protected int binSearchSplits(String key)
+	{
+		int loc = Collections.binarySearch(splitList, key);
+		if (loc < 0) {
+			loc = (loc * -1) - 2;
+			if (loc < 0) {
+				loc = 0;
+			}
+		}
+		
+		return loc;
+	}
+	
+	protected void loadSplits(int numSplits)
+	{
+		CloseableIterator<String> splitIter = null;
+		
+		try {
+			splitIter = summary.getSplitIterator(EMPTY_STRING, EMPTY_STRING, numSplits);
+			
+			splitList = new ArrayList<String>();
+			
+			while (splitIter.hasNext()) {
+				String str = splitIter.next();
+				int keyEndIndex = str.indexOf(' ');
+				if (keyEndIndex >= 0) {
+					str = str.substring(0, keyEndIndex);
+				}
+				splitList.add(str);
+				System.out.println(str);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (splitIter != null) {
+				try {
+					splitIter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void configure(JobConf conf) {
+		String clusterSummary = conf.get(ZIPNUM_PARTITIONER_CLUSTER);
+		
+		try {
+			summary = new SortedTextFile(clusterSummary);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
