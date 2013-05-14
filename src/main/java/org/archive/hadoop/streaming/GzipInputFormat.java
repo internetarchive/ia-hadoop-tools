727f617be9830d195c724f05b0acde7f566cea60
Ilya Kreymer <ilya@archive.org>
2013-05-14 12:59:12 -0700

    gzipinputformat: Using CombineFileInputFormat to combine small gz together

--- src/main/java/org/archive/hadoop/streaming/GzipInputFormat.java
+++ src/main/java/org/archive/hadoop/streaming/GzipInputFormat.java
@@ -2,23 +2,23 @@ package org.archive.hadoop.streaming;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Vector;
 
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapred.InputSplit;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.LineRecordReader;
-import org.apache.hadoop.mapred.LineRecordReader.LineReader;
 import org.apache.hadoop.mapred.RecordReader;
 import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.archive.util.zip.OpenJDK7GZIPInputStream;
+import org.apache.hadoop.mapred.lib.CombineFileInputFormat;
+import org.apache.hadoop.mapred.lib.CombineFileSplit;
 
-public class GzipInputFormat extends TextInputFormat {
+public class GzipInputFormat extends CombineFileInputFormat<LongWritable, Text> {
 
 	@Override
 	protected boolean isSplitable(FileSystem fs, Path file) {
@@ -30,28 +30,37 @@ public class GzipInputFormat extends TextInputFormat {
 			InputSplit genericSplit, JobConf job, Reporter reporter)
 			throws IOException {
 		
-		FileSplit split = (FileSplit)genericSplit;
+		CombineFileSplit split = (CombineFileSplit)genericSplit;
 		
-		long start = split.getStart();
-		long end = start + split.getLength();
-		final Path file = split.getPath();
+		Vector<InputStream> vec = new Vector<InputStream>();
 		
-//		compressionCodecs = new CompressionCodecFactory(job);
-//		final CompressionCodec codec = compressionCodecs.getCodec(file);
-
-		// open the file and seek to the start of the split
-		FileSystem fs = file.getFileSystem(job);
-		FSDataInputStream fileIn = fs.open(split.getPath());
-		
-		boolean skipFirstLine = false;
-		
-		InputStream in = new OpenJDK7GZIPInputStream(fileIn);
-		
-		if (start != 0) {
-			skipFirstLine = true;
-			--start;
-			fileIn.seek(start);
+		for (Path path : split.getPaths()) {
+			FileSystem fs = path.getFileSystem(job);
+			FSDataInputStream fileIn = fs.open(path);
+			vec.add(fileIn);
 		}
+		
+		InputStream in = new SequenceInputStream(vec.elements());
+		
+//		long start = split.getStart();
+//		long end = start + split.getLength();
+//		final Path file = split.getPath();
+//		
+////		compressionCodecs = new CompressionCodecFactory(job);
+////		final CompressionCodec codec = compressionCodecs.getCodec(file);
+//
+//		// open the file and seek to the start of the split
+//		FSDataInputStream fileIn = fs.open(split.getPath());
+//		
+//		boolean skipFirstLine = false;
+//		
+//		InputStream in = new OpenJDK7GZIPInputStream(fileIn);
+//		
+//		if (start != 0) {
+//			skipFirstLine = true;
+//			--start;
+//			fileIn.seek(start);
+//		}
 		
 	    String delimiter = job.get("textinputformat.record.delimiter");
 	    byte[] recordDelimiter = null;
@@ -59,13 +68,13 @@ public class GzipInputFormat extends TextInputFormat {
 	    	recordDelimiter = delimiter.getBytes();
 	    }
 		
-		if (skipFirstLine) { // skip first line and re-establish "start".
-			LineRecordReader.LineReader reader = new LineReader(in, job, recordDelimiter);
-			start += reader.readLine(new Text(), 0,
-					(int) Math.min((long) Integer.MAX_VALUE, end - start));
-		}		
+//		if (skipFirstLine) { // skip first line and re-establish "start".
+//			LineRecordReader.LineReader reader = new LineReader(in, job, recordDelimiter);
+//			start += reader.readLine(new Text(), 0,
+//					(int) Math.min((long) Integer.MAX_VALUE, end - start));
+//		}		
 		
-		return new LineRecordReader(in, start, end, job, recordDelimiter);
+		return new LineRecordReader(in, 0, Long.MAX_VALUE, job, recordDelimiter);
 
 	}
 }
