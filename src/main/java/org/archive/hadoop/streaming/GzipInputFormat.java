ac9d2a32bcff3e2de281c4ce2fa81312c546531e
Ilya Kreymer <ilya@archive.org>
2013-05-15 13:02:17 -0700

    GzipInputFormat fault tolerance, gracefully no-op after 2 fail attempts

--- src/main/java/org/archive/hadoop/streaming/GzipInputFormat.java
+++ src/main/java/org/archive/hadoop/streaming/GzipInputFormat.java
@@ -14,12 +14,15 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.LineRecordReader;
 import org.apache.hadoop.mapred.RecordReader;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TaskAttemptID;
 import org.apache.hadoop.mapred.lib.CombineFileInputFormat;
 import org.apache.hadoop.mapred.lib.CombineFileRecordReader;
 import org.apache.hadoop.mapred.lib.CombineFileSplit;
 import org.archive.util.zip.OpenJDK7GZIPInputStream;
 
 public class GzipInputFormat extends CombineFileInputFormat<Text, Text> {
+	
+	final static int MAX_ALLOW_FAILURES = 2;
 
 	@Override
 	protected boolean isSplitable(FileSystem fs, Path file) {
@@ -36,35 +39,61 @@ public class GzipInputFormat extends CombineFileInputFormat<Text, Text> {
 	
 	public static class GzipSingleFileRecordReader implements RecordReader<Text, Text>
 	{
-		final LineRecordReader reader;
+		LineRecordReader reader;
 		LongWritable longKey;
+		String currPath;
+		int currAttempt;
 		
 		public GzipSingleFileRecordReader(CombineFileSplit split, Configuration conf, Reporter reporter, Integer index) throws IOException
 		{
 			final Path file = split.getPath(index);
+			currPath = file.toString();
 			
-			FileSystem fs = file.getFileSystem(conf);
-
-			FSDataInputStream fileIn = fs.open(file);
-						
-		    String delimiter = conf.get("textinputformat.record.delimiter");
-		    
-		    byte[] recordDelimiter = null;
-		    
-		    if (null != delimiter) {
-		    	recordDelimiter = delimiter.getBytes();
-		    }
-		    
-		    InputStream in = new OpenJDK7GZIPInputStream(fileIn);
-		    
-			reader = new LineRecordReader(in, 0, Long.MAX_VALUE, conf, recordDelimiter);
-			longKey = reader.createKey();
+			currAttempt = TaskAttemptID.forName(conf.get("mapred.task.id")).getId();
+			
+			try {
+			
+				FileSystem fs = file.getFileSystem(conf);
+	
+				FSDataInputStream fileIn = fs.open(file);
+							
+			    String delimiter = conf.get("textinputformat.record.delimiter");
+			    
+			    byte[] recordDelimiter = null;
+			    
+			    if (null != delimiter) {
+			    	recordDelimiter = delimiter.getBytes();
+			    }
+			    
+			    InputStream in = new OpenJDK7GZIPInputStream(fileIn);
+			    
+				reader = new LineRecordReader(in, 0, Long.MAX_VALUE, conf, recordDelimiter);
+				longKey = reader.createKey();
+			} catch (IOException io) {
+				if (currAttempt < MAX_ALLOW_FAILURES) {
+					throw io;
+				}
+			}
 		}
 
 		@Override
 		public boolean next(Text key, Text value) throws IOException {
-			value.set("");
-			return reader.next(longKey, key);
+			
+			if (reader == null) {
+				return false;
+			}
+			
+			try {
+				value.clear();
+				return reader.next(longKey, key);
+			} catch (IOException io) {
+				if (currAttempt < MAX_ALLOW_FAILURES) {
+					throw new IOException("Failed reading from " + currPath, io);	
+				} else {
+					key.clear();
+					return false;
+				}
+			}
 		}
 
 		@Override
