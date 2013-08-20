80cd1b709323620efcc0475cc25cfa7f0a2dcc93
Ilya Kreymer <ilya@archive.org>
2013-08-20 14:40:30 -0700

    hadoop-tools: add ias3

---
+++ src/main/java/org/archive/hadoop/fs/IAS3.java
@@ -0,0 +1,28 @@
+package org.archive.hadoop.fs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.fs.s3native.NativeS3FileSystem;
+import org.jets3t.service.S3ServiceException;
+
+public class IAS3 extends NativeS3FileSystem {
+
+	// Suppress S3ServiceException
+	@Override
+    public boolean mkdirs(Path f, FsPermission permission) throws IOException {
+		boolean result = false;
+		
+		try {
+			result = super.mkdirs(f, permission);
+		} catch (IOException io) {
+			System.out.println(io.getCause());
+			if (io.getCause() instanceof S3ServiceException) {
+				result = true;
+			}
+		}
+		
+		return result;
+	}
+}
